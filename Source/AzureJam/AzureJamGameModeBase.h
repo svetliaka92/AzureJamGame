58c57051c08649e7709a91190dd3df373f73be17
SSVILENO <SSVILENO@ford.com>
2021-09-10 19:07:43 +0200

    Project added
    - Created project
    - Added project to repository

---
+++ Source/AzureJam/AzureJamGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AzureJamGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AZUREJAM_API AAzureJamGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
